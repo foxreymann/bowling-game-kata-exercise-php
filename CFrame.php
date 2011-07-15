007655cfd37cc8037b6ce5d81be44a9b5bc4496e
tomaszjureczko <tomasz.jureczko@gmail.com>
2011-07-15 21:06:44 +0200

    declatarion of first two classes

---
+++ CFrame.php
@@ -0,0 +1,17 @@
+<?php
+
+/* Points from one frame */
+class CFrame {
+    $m_aPoints = [];
+    $m_iBonusDue;  
+
+    function fGetScore() {
+       return 0;
+    }
+
+    function fNextRollDue() {
+       return false;
+    }     
+} 
+
+?>
---
+++ CGameManager.php
@@ -0,0 +1,11 @@
+<?php
+
+/* Manages the number of rounds and throws */
+class CGameManager {
+
+    $m_iFrame = 1;
+    $m_iRoll = 1;
+
+} 
+
+?>
